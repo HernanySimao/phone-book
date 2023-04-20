f5e44424fdc35ee321009abef1ca60043606ee0e
HernanySimao <hernanysimao123@gmail.com>
2023-04-20 16:27:32 +0100

    preparando projecto

--- src/main.js
+++ src/main.js
@@ -2,5 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios';
+import "bootstrap/dist/css/bootstrap.css";
 
-createApp(App).use(store).use(router).mount('#app')
+const axiosInstance = axios.create({
+    baseURL: 'https://api.github.com',
+});
+const app = createApp(App);
+app.config.globalProperties.$axios = axiosInstance;
+
+app.use(store).use(router).mount('#app')
---
+++ src/views/home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Olá, eu sou o seu professor</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  data() {
+    return {};
+  },
+
+  mounted() {
+    this.teste();
+  },
+
+  methods: {
+    async teste() {
+      await this.$axios.get("/users/HernanySimao").then((data) => {
+        console.log(data);
+      });
+    },
+  },
+};
+</script>
